316eaf9ddbc28e2ad5e65a7210d91149a28a8709
Heath Naylor <heath@bythepixel.com>
2017-12-04 14:23:01 -0700

    Push pdo exceptions into a bag instead of forcing a print
    Fix scoping of config member
    
    Make compatible with PHP 5+
    
    Generic bag
    
    Ensure consistent code style throughout
    
    Docblocks added
    
    Remove unused variables
    
    Allow sane default port
    
    Bring in server request interface and adhere to it within datatables
    
    Allow for bindings
    
    Fix tests

--- tests/spec/DatatablesSpec.php
+++ tests/spec/DatatablesSpec.php
@@ -6,22 +6,59 @@ use Ozdemir\Datatables\DB\MySQL;
 use Ozdemir\Datatables\DB\SQLite;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Zend\Diactoros\ServerRequest;
 
-class DatatablesSpec extends ObjectBehavior {
+/**
+ * Class DatatablesSpec
+ * @package spec\Ozdemir\Datatables
+ */
+class DatatablesSpec extends ObjectBehavior
+{
 
     function let()
     {
         $sqlconfig = realpath(dirname(__FILE__) . '/test.db');
         $db = new SQLite($sqlconfig);
 
-        $this->beConstructedWith($db);
+        $request = new ServerRequest();
+        $request = $request->withParsedBody([
+            'draw' => '2',
+            'columns' => [
+                0 => [
+                    'data' => 'Test Column',
+                    'name' => '',
+                    'searchable' => 'true',
+                    'orderable' => 'true',
+                    'search' => [
+                        'value' => '',
+                        'regex' => false,
+                    ],
+                ],
+            ],
+            'order' => [
+                0 => [
+                    'column' => "0",
+                    'dir' => 'asc',
+                ],
+            ],
+            'start' => '0',
+            'length' => '20',
+            'search' => [
+                'value' => '',
+                'regex' => 'false',
+            ],
+        ]);
+
+        $this->beConstructedWith($db, $request);
     }
 
+    /**
+     * @return array
+     */
     public function getMatchers()
     {
         return [
-            'haveColumns' => function ($subject, $key)
-            {
+            'haveColumns' => function ($subject, $key) {
                 return (array_keys($subject) === $key);
             }
         ];
@@ -29,7 +66,7 @@ class DatatablesSpec extends ObjectBehavior {
 
     public function it_returns_record_counts()
     {
-        $this->query("Select id as fid, name, surname, age from mytable where id > 3");
+        $this->query("SELECT id AS fid, name, surname, age FROM mytable WHERE id > 3");
         $datatables = $this->generate(false);
         $datatables['recordsTotal']->shouldReturn(8);
         $datatables['recordsFiltered']->shouldReturn(8);
@@ -37,7 +74,7 @@ class DatatablesSpec extends ObjectBehavior {
 
     public function it_returns_data_from_a_basic_sql()
     {
-        $this->query("Select id as fid, name, surname, age from mytable");
+        $this->query("SELECT id AS fid, name, surname, age FROM mytable");
 
         $data = $this->generate(false)['data'][0];
 
@@ -48,20 +85,20 @@ class DatatablesSpec extends ObjectBehavior {
 
     public function it_sets_column_names_from_aliases()
     {
-        $this->query("Select
-                  film_id as fid,
+        $this->query("SELECT
+                  film_id AS fid,
                   title,
-                  'description' as info,
+                  'description' AS info,
                   release_year 'r_year',
                   film.rental_rate,
-                  film.length as mins
-            from film");
+                  film.length AS mins
+            FROM film");
         $this->get('all_columns')->shouldReturn(['fid', 'title', 'info', 'r_year', 'rental_rate', 'mins']);
     }
 
     public function it_hides_unnecessary_columns_from_output()
     {
-        $this->query("Select id as fid, name, surname, age from mytable");
+        $this->query("SELECT id AS fid, name, surname, age FROM mytable");
         $this->hide('fid');
         $data = $this->generate(false)['data']['2'];
 
@@ -72,15 +109,13 @@ class DatatablesSpec extends ObjectBehavior {
 
     public function it_returns_modified_data_via_closure_function()
     {
-        $this->query("Select id as fid, name, surname, age from mytable");
+        $this->query("SELECT id AS fid, name, surname, age FROM mytable");
 
-        $this->edit('name', function ($data)
-        {
+        $this->edit('name', function ($data) {
             return strtolower($data['name']);
         });
 
-        $this->edit('surname', function ($data)
-        {
+        $this->edit('surname', function ($data) {
             return $this->customfunction($data['surname']);
         });
 
@@ -115,11 +150,11 @@ class DatatablesSpec extends ObjectBehavior {
         $dt = $this->query("SELECT column_name
             FROM COLUMNS
             WHERE table_schema = 'mysql' AND table_name = 'user'
-            and (SELECT group_concat(cp.GRANTEE)
+            AND (SELECT group_concat(cp.GRANTEE)
             FROM COLUMN_PRIVILEGES cp
             WHERE cp.TABLE_SCHEMA = COLUMNS.TABLE_SCHEMA
             AND cp.TABLE_NAME = COLUMNS.TABLE_NAME
-            AND cp.COLUMN_NAME = COLUMNS.COLUMN_NAME) is not null;");
+            AND cp.COLUMN_NAME = COLUMNS.COLUMN_NAME) IS NOT NULL;");
 
         $dt->get('columns')->shouldReturn(['column_name']);
     }
